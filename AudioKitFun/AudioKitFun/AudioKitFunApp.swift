4f35abe6d2e91bfaa3970ecb801bc8f538d18ef5
Ben Scheirman <ben@scheirman.com>
2025-04-04 08:54:58 -0500

    Episode 591

---
+++ AudioKitFun/AudioKitFun/AudioKitFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioKitFunApp.swift
+//  AudioKitFun
+//
+//  Created by Ben Scheirman on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AudioKitFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
